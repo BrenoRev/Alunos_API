63dc78b4f0c78abc785592ebf73df9bd3f9b22a3
BrenoRev <silvabreno462@gmail.com>
2022-04-11 20:33:54 -0300

    CRUD Usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,6 +14,62 @@ class UserController {
         }
      
     }
+
+    async index(req, res) {
+        try{
+            const users = await User.findAll();
+            return res.json(users)
+        } catch(e) {
+           return res.status(400).json(
+                { 
+                  errors:  e.errors.map(error => error.message)
+                }
+                )
+        }
+    }
+
+    async show(req, res) {
+        try{
+            const user = await User.findByPk(req.params.id);
+            return res.json(user)
+        } catch(e){
+            return res.status(400).json(
+                { 
+                  errors:  e.errors.map(error => error.message)
+                }
+                )
+        }
+    }
+
+    async update(req, res) {
+        try{
+            const user = await User.findByPk(req.params.id);
+            await user.update(req.body);
+            return res.json(user)
+        } catch(e) {
+            return res.status(400).json(
+                { 
+                  errors:  e.errors.map(error => error.message)
+                }
+                )
+        }
+
+    }
+
+    async destroy(req, res) {
+        try{
+            const user = await User.findByPk(req.params.id);
+            await user.destroy();
+            return res.json(user)
+        } catch(e) {
+            return res.status(400).json(
+                { 
+                  errors:  e.errors.map(error => error.message)
+                }
+                )
+        }
+    }
+
 }
 
 export default new UserController();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,10 @@ const router = new Router();
 import userController from '../controllers/userController';
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.destroy);
+
 
 export default router;
