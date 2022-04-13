74c3dc99fc7ed3727f3f7e507d482d745c4baade
BrenoRev <silvabreno462@gmail.com>
2022-04-13 01:39:46 -0300

    Tratando os arquivos enviados

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,11 +1,7 @@
 import { Router } from 'express';
-import multer from 'multer'
-import multerConfig from '../config/multer'
 import fotoController from '../controllers/FotoController';
-
 const router = new Router();
-const upload = multer(multerConfig);
 
-router.post('/', upload.single('foto'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
