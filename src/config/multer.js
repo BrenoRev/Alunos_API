b4e5e2b39a6de187190c8b89010fc0c690b8dec5
BrenoRev <silvabreno462@gmail.com>
2022-05-18 20:46:28 -0300

    hotfix/images

--- src/config/multer.js
+++ src/config/multer.js
@@ -14,10 +14,10 @@ export default {
     },
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, resolve(__dirname, '..', '..', 'uploads'))
+            cb(null, resolve(__dirname, '..', '..', 'uploads', 'images'))
         },
         filename: (req, file, cb) => {
-            cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`)
+            cb(null, `${file.originalname}`)
         }
     })
 }
